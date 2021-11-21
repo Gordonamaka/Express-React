3bb13973a618cece0d2b5994c6e4195b70eae0a8
Gordonamaka <amobimak@gmail.com>
2021-11-21 17:09:12 -0500

    setup before data abstraction

---
+++ controllers/dataControllers.js
@@ -0,0 +1,5 @@
+exports.dataController = (req, res) => {
+
+
+
+}
--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/users/", require("./routes/usersRoute"));
 
+app.use("/data/", require("./routes/dataRoute"));
 
 app.listen(3001, function() {
   console.log('Express server is running on port 3001');
-})
+});
