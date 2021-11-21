82b3bc641ca9144328600f1ca06ea7d3b042163a
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-11-21 22:53:31 +0000

    database and .env added

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 
 app.use("/users/", require("./routes/usersRoute"));
 
 
-app.listen(3001, function() {
-  console.log('Express server is running on port 3001');
-})
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
