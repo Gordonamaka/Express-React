a34e83e8d560f94916f754d6758e00a9afce0606
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-11-22 23:28:33 -0500

    Added db reset and finalized seeding

---
+++ db/schema/04_daily_stock_data.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS daily_stock_data CASCADE;
+CREATE TABLE daily_stock_data (
+  id SERIAL PRIMARY KEY NOT NULL,
+  ticker VARCHAR(255) NOT NULL,
+  interval VARCHAR(255) NOT NULL,
+  time VARCHAR(255) NOT NULL,
+  open VARCHAR(255) NOT NULL,
+  high VARCHAR(255) NOT NULL,
+  low VARCHAR(255) NOT NULL,
+  close VARCHAR(255) NOT NULL,
+  adjusted_close VARCHAR(255) NOT NULL,
+  volume VARCHAR(255) NOT NULL,
+  dividend_amount VARCHAR(255) NOT NULL,
+  split_coefficient VARCHAR(255) NOT NULL
+);
+
+
